7f05e88f0fe338edf68e064dc6c6e7d376c4e1c9
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-06 12:09:25 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -13,9 +13,9 @@ let starsInSky = 20;
 // 3.
 console.log(atoms>starsInSky&&atoms>sandGrains);
 //3.a
-console.log(atoms!=sandGrains);
-console.log(starsInSky<sandGrains || starsInSky > atoms)
-console.log(atoms == starsInSky || atoms != sandGrains)
-console.log(atoms>=10 && sandGrains<=10)
+console.log(atoms!==sandGrains);
+console.log(starsInSky<sandGrains || starsInSky > atoms);
+console.log(atoms == starsInSky||atoms!==sandGrains);
+console.log(atoms>=10&&sandGrains<=10);
 console.log(atoms*starsInSky<100 || atoms*sandGrains>100);
  
