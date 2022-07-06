d7b8b56ac9cfd9869b2e2f86b9fceb73573811b3
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-06 11:57:23 +0200

    Update solution.js

---
+++ solution.js
@@ -0,0 +1,21 @@
+// 1. 
+let isDogBetter = true;
+let isCatBetter = false;
+console.log(isDogBetter && isCatBetter);
+console.log(isDogBetter || isCatBetter);
+console.log(!(isDogBetter && isCatBetter));
+
+// 2.
+let atoms = 900;
+let sandGrains = 10;
+let starsInSky = 20;
+
+// 3.
+console.log(atoms>starsInSky&&atoms>sandGrains);
+//3.a
+console.log(atoms!=sandGrains);
+console.log(starsInSky<sandGrains || starsInSky > atoms)
+console.log(atoms == starsInSky || atoms != sandGrains)
+console.log(atoms>=10 && sandGrains<=10)
+console.log(atoms*starsInSky<100 || atoms*sandGrains>100);
+ 
